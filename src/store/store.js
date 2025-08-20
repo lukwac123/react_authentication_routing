2ed309a9e9871c310d59f8a502c27b121924b702
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-08-20 14:44:40 +0200

    Store aplikacji

---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { applyMiddleware, createStore } from "redux";
+import reducers from "../reducers/index";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
+const store = createStore(reducers, applyMiddleware(...middlewares));
+export { store };
